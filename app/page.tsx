baa527cc90685b1238ab66bbe358ac9a0ca3e3db
odinaka-dev <uchechukwupeter22@gmail.com>
2025-06-27 11:33:46 +0100

    refactor: simplify MainPage by removing React Query integration and unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,18 @@
-"use client";
-
 import React from "react";
 
 import { Metadata } from "next";
 import { Main } from "@/exports/exports";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-// export const metadata: Metadata = {
-//   title: "wazzap homepage",
-//   description:
-//     "explore our latest features and updates that connects you with the world",
-// };
+export const metadata: Metadata = {
+  title: "wazzap homepage",
+  description:
+    "explore our latest features and updates that connects you with the world",
+};
 
 export default function MainPage() {
-  const queryClient = new QueryClient();
-
   return (
     <div>
-      <QueryClientProvider client={queryClient}>
-        <Main />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <Main />
     </div>
   );
 }
