5262781c8d5ecf54d14495b3f45dec535521aea4
odinaka-dev <uchechukwupeter22@gmail.com>
2025-06-27 11:25:44 +0100

    refactor: integrate React Query in MainPage and clean up RootProviders

--- providers/root-provider.tsx
+++ providers/root-provider.tsx
@@ -3,9 +3,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-// import { getQueryClient } from "@/app/get-query-client";
-// import { ErrorListener, InitMixpanel } from "@/components/shared";
-
 export default function RootProviders({
   children,
 }: {
