37925c5965bad282af278c15edc45b7a41697f96
odinaka-dev <uchechukwupeter22@gmail.com>
2025-06-30 09:39:03 +0100

    fix: enhance button styling in SignupForm for better focus visibility

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -157,7 +157,10 @@ export function SignupForm({
               required
             />
           </div>
-          <Button type="submit" className="w-full  bg-[#634aff]">
+          <Button
+            type="submit"
+            className="w-full  bg-[#634aff] focus:bg-gray-600"
+          >
             Sign Up
           </Button>
           <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
